767182ffc8c114c5d142b2c1b1838ff28827bd78
crossxyz <46918630+crossxyz@users.noreply.github.com>
2024-02-29 18:36:17 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6540506696108321" crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6540506696108321" crossOrigin="anonymous"></script>
       </head>
       <body>
         {children}
